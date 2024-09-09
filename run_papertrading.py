fdc06cb770b9550ffe0f397a73ac1b80f2ca67ba
Dân <nguyenandan@gmail.com>
2024-09-09 13:27:56 +0700

    Plutus integration without structure change

--- run_papertrading.py
+++ run_papertrading.py
@@ -1,15 +1,16 @@
-import redis
+import time
+
 import json
 import pytz
 from datetime import datetime
 from pathlib import Path
-
 
 from src.pipeline import Pipeline
 from utils.date_management import make_date_to_tickersymbol
 from utils.argument_management import Opts
 from utils.file_management import load_yaml
 
+from plutus.datahub.redis_datahub import RedisDataHub
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]
@@ -18,47 +19,40 @@ TIMEZONE = pytz.timezone('Asia/Ho_Chi_Minh')
 
 # handle pricehub quote
 def redis_message_handler(redis_message):
+    quote = json.loads(redis_message['data'])
+    cur_price = quote['latest_matched_price']
 
-  quote = json.loads(redis_message['data'])
-  cur_price = quote['latest_matched_price']
+    # check if cur_price updated yet
+    if cur_price is None:
+        return
 
-  # check if cur_price updated yet
-  if cur_price is None:
-    return
-
-  datetime_now = datetime.fromtimestamp(quote['timestamp']).astimezone(TIMEZONE).time()
-
-
-  
-  
-
+    datetime_now = datetime.fromtimestamp(quote['timestamp']).astimezone(TIMEZONE).time()
 
 
 def main():
     FLAGS = Opts().parse_args()
     pipeline = Pipeline(FLAGS)
-    
+
     redis_opts = load_yaml(ROOT/'configs'/'usr'/'redis_account.yaml')
     redis_host, redis_port, redis_password = redis_opts['host'], redis_opts['port'], redis_opts['password']
     print(redis_host, redis_port, redis_password)
-    
+
     # connect to redis server
-    redis_client = redis.Redis(
-    host=redis_host,
-    port=redis_port,
-    password=redis_password
+    redis_data_hub = RedisDataHub(
+        redis_host=redis_host,
+        redis_port=redis_port,
+        redis_password=redis_password
     )
 
-    # check connection to redis OK
-    print(redis_client.ping())
-
     current_date = datetime.now()
-    tickersymbol = make_date_to_tickersymbol(current_date)
-    F1M_CHANNEL = f'HNXDS:{tickersymbol}'
+    ticker_symbol = make_date_to_tickersymbol(current_date)
+    F1M_CHANNEL = f'HNXDS:{ticker_symbol}'
     print(F1M_CHANNEL)
-    
-    pipeline.run_papertrading(redis_client)
+
+    pipeline.run_papertrading(redis_data_hub)
+
 
 if __name__ == "__main__":
     main()
-    
+    while True:
+        time.sleep(0.01)
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,10 @@
 import os
 import time
 import pytz
-import json
 import logging
-import optuna
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from pathlib import Path
 
 from src.bot.bot import Bot
 from src.optimizers.bruteforce_optimizer import BruteForceOptimizer
@@ -20,7 +17,12 @@ from utils.date_management import make_date_to_tickersymbol
 from utils.path_management import increment_path
 from utils.file_management import write_yaml
 
+from plutus.core.instrument import Instrument
+from plutus.datahub.redis_datahub import RedisDataHub, RedisDataHandler, InternalDataHubQuote
+
 TIMEZONE = pytz.timezone('Asia/Ho_Chi_Minh')
+
+
 class Pipeline():
     def __init__(self, opts):
         self.opts = opts
@@ -29,10 +31,15 @@ class Pipeline():
             self.val_data = load_csv(opts['DATASET']['VAL']['csv_file'])
             self.test_data = load_csv(opts['DATASET']['TEST']['csv_file'])
 
+        self.logger = None
+        self.current_symbol = None
+        self.model = None
+        self.visualizer = None
+
     def _init_logging(self, log_file='log.txt', name='logger'):
         """ Initialize logging
         """
-        
+
         logger = logging.getLogger(name)
         logger.setLevel(logging.DEBUG)
 
@@ -54,15 +61,14 @@ class Pipeline():
     def calculate_performance_score(self, returns, num_order, fees=0.3):
         profit_array = np.array(returns)
         max_drawdown_value = maximum_drawdown(profit_array)
-        profit = np.sum(returns - (num_order)*fees)
+        profit = np.sum(returns - (num_order) * fees)
         sharpe = sharpe_ratio(returns)
         return profit, sharpe, max_drawdown_value
-    
 
     def _log_results(self, logger, model, symbol):
         monthlyAvgSpread, monthlyProfit, num_order = model.get_monthly_history().get_statistic()
-        profit, sharpe, max_drawdown_value = self.calculate_performance_score(monthlyProfit, 
-                                                                              num_order, 
+        profit, sharpe, max_drawdown_value = self.calculate_performance_score(monthlyProfit,
+                                                                              num_order,
                                                                               self.opts['PIPELINE']['params']['fee'])
 
         logger.info("                   Results                   ")
@@ -79,29 +85,33 @@ class Pipeline():
 
     def report_monthly_data(self, model, save_dir=None):
         os.makedirs(save_dir, exist_ok=True)
-        model.get_monthly_history().export_df_inventory(save_file=save_dir/'inventory.csv')
-        df_order_analysis = model.get_monthly_history().export_df_order_analysis(save_file=save_dir/'order.csv')
-        model.get_monthly_history().export_df_profit_per_day(save_file=save_dir/'profit.csv')
-        bids,asks = model.get_monthly_history().get_bidsask_spread()
+        model.get_monthly_history().export_df_inventory(save_file=save_dir / 'inventory.csv')
+        df_order_analysis = model.get_monthly_history().export_df_order_analysis(save_file=save_dir / 'order.csv')
+        model.get_monthly_history().export_df_profit_per_day(save_file=save_dir / 'profit.csv')
+        bids, asks = model.get_monthly_history().get_bidsask_spread()
         df = model.monthly_tick_data.export_df_market_timeprice()
-        df_spread = pd.DataFrame({'datetime':df['datetime'],'ask': asks, 'bid': bids})
-        df_spread.to_csv(save_dir/'ask_bid.csv', index=False)
+        df_spread = pd.DataFrame({'datetime': df['datetime'], 'ask': asks, 'bid': bids})
+        df_spread.to_csv(save_dir / 'ask_bid.csv', index=False)
 
     def export_df_result(self, model, save_dir=None):
         totalAvgSpread, totalProfit, num_order = model.get_total_history().get_statistic()
 
-        profit, sharpe, max_drawdown_value = self.calculate_performance_score(totalProfit, 
-                                                                              num_order,
-                                                                              self.opts['PIPELINE']['params']['fee'])
+        profit, sharpe, max_drawdown_value = self.calculate_performance_score(
+            totalProfit,
+            num_order,
+            self.opts['PIPELINE']['params']['fee']
+        )
 
-        df = pd.DataFrame({'avg_spread': [totalAvgSpread],
-                           'num_trade': [num_order.sum()],
-                           'profit': [profit],
-                           'sharpe_ratio': [sharpe],
-                           'max_drawdown': [max_drawdown_value]})
+        df = pd.DataFrame({
+            'avg_spread': [totalAvgSpread],
+            'num_trade': [num_order.sum()],
+            'profit': [profit],
+            'sharpe_ratio': [sharpe],
+            'max_drawdown': [max_drawdown_value]
+        })
 
         if save_dir:
-            df.to_csv(save_dir/'result.csv', index=False)
+            df.to_csv(save_dir / 'result.csv', index=False)
         return df
 
     def optuna_optimizing(self, datasets):
@@ -109,17 +119,21 @@ class Pipeline():
         num_of_spread_list = self.opts['OPTIMIZER']['params']['num_of_spread']['values']
         historical_window_size_list = self.opts['OPTIMIZER']['params']['historical_window_size']['values']
         min_second_time_step_list = self.opts['OPTIMIZER']['params']['min_second_time_step']['values']
+
         def objective(trial):
             self.opts['PIPELINE']['params']['gamma'] = trial.suggest_categorical('gamma', gamma_list)
-            self.opts['PIPELINE']['params']['num_of_spread'] = trial.suggest_categorical('num_of_spread', num_of_spread_list)
-            self.opts['PIPELINE']['params']['historical_window_size'] = trial.suggest_categorical('historical_window_size', historical_window_size_list)
-            self.opts['PIPELINE']['params']['min_second_time_step'] = trial.suggest_categorical('min_second_time_step', min_second_time_step_list)
+            self.opts['PIPELINE']['params']['num_of_spread'] = trial.suggest_categorical('num_of_spread',
+                                                                                         num_of_spread_list)
+            self.opts['PIPELINE']['params']['historical_window_size'] = trial.suggest_categorical(
+                'historical_window_size', historical_window_size_list)
+            self.opts['PIPELINE']['params']['min_second_time_step'] = trial.suggest_categorical('min_second_time_step',
+                                                                                                min_second_time_step_list)
 
             _, sharpe, _ = self.run_dataset(datasets, type_data='train', is_visualize=False)
             return sharpe
-        
+
         return objective
-    
+
     def fit(self):
         best_params = None
         # optimization
@@ -130,33 +144,33 @@ class Pipeline():
         # Start fitting with best params
         self.run_dataset(self.train_data, type_data='train', params=best_params)
         self.run_dataset(self.val_data, type_data='val', params=best_params)
-          
 
     def run_dataset(self, datasets, type_data='train', is_visualize=True, params=None, prefix_logger=""):
         tmp_opts = self.opts.copy()
         save_dir = tmp_opts['PIPELINE']['params']['save_dir']
         if "exp" in str(save_dir):
             save_dir = save_dir.parents[0]
-        save_dir = increment_path(save_dir/"exp", mkdir=True)
+        save_dir = increment_path(save_dir / "exp", mkdir=True)
         tmp_opts['PIPELINE']['params']['save_dir'] = save_dir
 
-        write_yaml(tmp_opts, save_dir/'config.yaml')
-        
+        write_yaml(tmp_opts, save_dir / 'config.yaml')
+
         if params:
             tmp_opts['PIPELINE']['params']['gamma'] = params['gamma']
             tmp_opts['PIPELINE']['params']['num_of_spread'] = params['num_of_spread']
             tmp_opts['PIPELINE']['params']['historical_window_size'] = params['historical_window_size']
             tmp_opts['PIPELINE']['params']['min_second_time_step'] = params['min_second_time_step']
-        
+
         save_dir = tmp_opts['PIPELINE']['params']['save_dir']
-        name_logger = prefix_logger+str(save_dir.parents[0].stem)
-        name_logger = f"{type_data}_logger_{name_logger}" 
-        
-        os.makedirs(save_dir/type_data, exist_ok=True)
-        logger = self._init_logging(tmp_opts['PIPELINE']['params']['save_dir']/type_data/'log.txt', name=name_logger)
+        name_logger = prefix_logger + str(save_dir.parents[0].stem)
+        name_logger = f"{type_data}_logger_{name_logger}"
+
+        os.makedirs(save_dir / type_data, exist_ok=True)
+        logger = self._init_logging(tmp_opts['PIPELINE']['params']['save_dir'] / type_data / 'log.txt',
+                                    name=name_logger)
         visualizer = VISUALIZER(fees=tmp_opts['PIPELINE']['params']['fee'])
         distinct_symbols = datasets['tickersymbol'].unique()
-        
+
         model = Bot(tmp_opts['PIPELINE']['params'])
         logger.info("Start fitting model")
         logger.info("with parameters: ")
@@ -164,83 +178,87 @@ class Pipeline():
         for symbol in distinct_symbols:
             start_time_1m = time.time()
             monthly_data = datasets[datasets['tickersymbol'] == symbol].drop(['tickersymbol'], axis=1).to_numpy()
-            
+
             logger.info(f"Start fitting model for {symbol}")
             model.fit(monthly_data)
-            
+
             end_time_1m = time.time()
 
             logger.info(f"Execution time for {symbol}: {end_time_1m - start_time_1m}")
             logger.info("params: ", tmp_opts['PIPELINE']['params'])
-            self._log_results(logger,model, symbol)
-            
-            self.report_monthly_data(model, save_dir=tmp_opts['PIPELINE']['params']['save_dir']/type_data/symbol)
+            self._log_results(logger, model, symbol)
+
+            self.report_monthly_data(model, save_dir=tmp_opts['PIPELINE']['params']['save_dir'] / type_data / symbol)
             if is_visualize:
                 visualizer.visualize_monthly_data(bot_data=model.get_monthly_history(),
                                                   bot_data_market_time_price=model.monthly_tick_data,
                                                   symbol=symbol,
-                                                  save_dir=tmp_opts['PIPELINE']['params']['save_dir']/type_data)
-        
+                                                  save_dir=tmp_opts['PIPELINE']['params']['save_dir'] / type_data)
+
         name = f"trading_with_maximum_inventory{tmp_opts['PIPELINE']['params']['maximum_inventory']}_windowsize{tmp_opts['PIPELINE']['params']['historical_window_size']}_min_second_time_step{tmp_opts['PIPELINE']['params']['min_second_time_step']}"
         if is_visualize:
-            visualizer.visualize_total_data(model.total_history_data_order, 
-                                            tmp_opts['PIPELINE']['params']['save_dir']/type_data,
+            visualizer.visualize_total_data(model.total_history_data_order,
+                                            tmp_opts['PIPELINE']['params']['save_dir'] / type_data,
                                             name=name)
-        self.export_df_result(model, tmp_opts['PIPELINE']['params']['save_dir']/type_data)
+        self.export_df_result(model, tmp_opts['PIPELINE']['params']['save_dir'] / type_data)
 
         _, totalProfit, num_order = model.get_total_history().get_statistic()
 
-        profit, sharpe, max_drawdown_value = self.calculate_performance_score(totalProfit, 
+        profit, sharpe, max_drawdown_value = self.calculate_performance_score(totalProfit,
                                                                               num_order,
                                                                               tmp_opts['PIPELINE']['params']['fee'])
-        
+
         logger.info(f"result on {type_data} with profit_return {profit}    sharpe {sharpe}   mdd {max_drawdown_value}")
         return profit, sharpe, max_drawdown_value
 
-    
-    def run_papertrading(self, redis_client):
+    def data_handler_func(self, instrument: Instrument, internal_data_quote: InternalDataHubQuote):
+        cur_price = internal_data_quote.latest_matched_price
+
+        now = datetime.fromtimestamp(internal_data_quote.timestamp).astimezone(TIMEZONE)
+        if cur_price is None:
+            self.logger.info(f"There is no price update yet at {now.strftime('%Y-%m-%d %H:%M:%S')}")
+            return
+
+        instrument_str = str(instrument)
+
+        if self.current_symbol is None:
+            self.current_symbol = instrument_str
+            self.model.init_capacity_every_month()
+        elif self.current_symbol != instrument_str:
+            self.visualizer.visualize_monthly_data(
+                bot_data=self.model.get_monthly_history(),
+                bot_data_market_time_price=self.model.monthly_tick_data,
+                symbol=self.current_symbol,
+                save_dir=self.opts['PIPELINE']['params']['save_dir'] / 'papertrading'
+            )
+            self._log_results(self.logger, self.model, self.current_symbol)
+            self.report_monthly_data(
+                self.model,
+                save_dir=self.opts['PIPELINE']['params']['save_dir'] / 'papertrading' / self.current_symbol
+            )
+            self.model.init_capacity_every_month()
+
+        self.model.fit_tickdata(Tickdata(now, cur_price))
+
+    def run_papertrading(self, redis_datahub: RedisDataHub):
         os.makedirs(self.opts['PIPELINE']['params']['save_dir'], exist_ok=True)
-        logger = self._init_logging(self.opts['PIPELINE']['params']['save_dir']/f'papertrading_log.txt', name='papertrading_logger')
-        model = Bot(self.opts['PIPELINE']['params'], logger=logger)
-        visualizer = VISUALIZER(fees=self.opts['PIPELINE']['params']['fee'])
+        self.logger = self._init_logging(self.opts['PIPELINE']['params']['save_dir'] / f'papertrading_log.txt',
+                                         name='papertrading_logger')
+        self.model = Bot(self.opts['PIPELINE']['params'], logger=self.logger)
+        self.visualizer = VISUALIZER(fees=self.opts['PIPELINE']['params']['fee'])
         current_date = datetime.now()
-        tickersymbol = make_date_to_tickersymbol(current_date)
-        current_symbol = None
-        logger.info(f"Start papertrading with tickersymbol {tickersymbol}")
-        logger.info(f"with parameters: {self.opts['PIPELINE']['params']}")
-        def redis_message_handler(redis_message, current_symbol=current_symbol, model=model, logger=logger, visualizer=visualizer, tickersymbol=tickersymbol):
-            instrument_id = redis_message['channel'].decode('utf-8')
-            quote = json.loads(redis_message['data'])
-            cur_price = quote['latest_matched_price']
-            print(cur_price)
-            
-            now = datetime.fromtimestamp(quote['timestamp']).astimezone(TIMEZONE)
-            if cur_price is None:
-                logger.info(f"There is no price update yet at {now.strftime('%Y-%m-%d %H:%M:%S')}")
-                return
+        ticker_symbol = make_date_to_tickersymbol(current_date)
+        instrument = Instrument(ticker_symbol=ticker_symbol, exchange_code_str='HNXDS')
+        self.logger.info(f"Start papertrading with tickersymbol {ticker_symbol}")
+        self.logger.info(f"with parameters: {self.opts['PIPELINE']['params']}")
 
-            if current_symbol is None:
-                current_symbol = tickersymbol
-                model.init_capacity_every_month()
-            elif current_symbol != tickersymbol:
-                visualizer.visualize_monthly_data(bot_data=model.get_monthly_history(),
-                                                  bot_data_market_time_price=model.monthly_tick_data,
-                                                  symbol=current_symbol,
-                                                  save_dir=self.opts['PIPELINE']['params']['save_dir']/'papertrading')
-                self._log_results(logger,model, current_symbol)
-                self.report_monthly_data(model, save_dir=self.opts['PIPELINE']['params']['save_dir']/'papertrading'/current_symbol)
-                model.init_capacity_every_month()
+        f1m_channel_pattern = instrument.id
 
-            model.fit_tickdata(Tickdata(now, cur_price))
-
-        F1M_CHANNEL = f'HNXDS:{tickersymbol}'
-
-        pub_sub = redis_client.pubsub()
-        pub_sub.psubscribe(**{F1M_CHANNEL: redis_message_handler})
-        # subscribe to channel F1M channel
-        # register a callback function to handle message received from redis-server
-        pubsub_thread = pub_sub.run_in_thread(sleep_time=0.001)
-        while True:
-            time.sleep(0.01)
-
-            # pubsub_thread.stop()
+        data_handler = RedisDataHandler(
+            data_handler_function=self.data_handler_func,
+            subscribed_pattern=f1m_channel_pattern,
+            run_in_thread=True,
+            sleep_time=0.001,
+        )
+        redis_datahub.data_handler_list.append(data_handler)
+        redis_datahub.start_pubsub()
